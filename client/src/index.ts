9041de1a2fdef5e63b77baa30aa660ed051ca05b
neoquijo <neoquijo@pm.me>
2023-09-01 09:25:59 +0200

    dist folder patch

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,6 +3,7 @@ import * as path from 'path'
 
 const app = express()
 app.use(express.static(path.join(__dirname, '../', 'dist')));
+app.use(express.static(path.join(__dirname, '../', 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'src/', 'index.html'));
 });
